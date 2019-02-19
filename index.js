7a5783727fe35ea3e067645854883d9b44247b70
cmmccoy1996@gmail.com <cmmccoy2@asu.edu>
2019-02-19 12:04:00 -0700

    t

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 var nodemailer = require('nodemailer');
 var request=require('request');
-var reCAPTCHA = require('recaptcha2');
+//var reCAPTCHA = require('recaptcha2');
+var Recaptcha = require('express-recaptcha').Recaptcha;
  
-var recaptcha = new reCAPTCHA({
+/*var recaptcha = new reCAPTCHA({
   siteKey: '6LfgipIUAAAAAMguDwZkJwHA66qm-iJyywLcYa5m', // retrieved during setup
   secretKey: '6LfgipIUAAAAABljW3Zah2MB58dWBCu9srkeEljK' // retrieved during setup
-});
-//ADD recapcha
+});*/
+
+
+ 
+var recaptcha = new Recaptcha('6LfgipIUAAAAAMguDwZkJwHA66qm-iJyywLcYa5m','6LfgipIUAAAAABljW3Zah2MB58dWBCu9srkeEljK');
 
 const app=express();
 var PORT=3000;
@@ -21,7 +25,8 @@ if(process.env.PORT)
 }
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 
@@ -35,20 +40,14 @@ app.get('/',function (req,res) {
 
 
 
-app.post('/send', function (req, res) {
-	recaptcha.validateRequest(req)
-    .then(function(){
-      // validated and secure
-      res.json({formSubmit:true})
-    })
-    .catch(function(errorCodes){
-      // invalid
-      res.json({
-        formSubmit: false,
-        errors: recaptcha.translateErrors(errorCodes) // translate error codes to human readable text
-      });
-    });
-
+app.post('/send',recaptcha.middleware.verify, function (req, res) {
+	if (!req.recaptcha.error) {
+	    // success code
+	    res.send("success");
+	  } else {
+	    // error code
+	    res.send(req.recaptcha.error);
+	  }
 
 
 	/*var firstName=req.body.firstName;
