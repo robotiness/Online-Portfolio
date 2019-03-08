ab4d8f3e95f35f9d9b070ed3e3b14043e2cf6dd6
cmmccoy1996@gmail.com <cmmccoy2@asu.edu>
2019-03-08 10:48:57 -0700

    Added email opt in form

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ var request=require('request');
 //var reCAPTCHA = require('recaptcha2');
 var Recaptcha = require('express-recaptcha').Recaptcha;
 //  git add . && git commit -m "t" && git push heroku master
- 
-/*var recaptcha = new reCAPTCHA({
-  siteKey: '6LfgipIUAAAAAMguDwZkJwHA66qm-iJyywLcYa5m', // retrieved during setup
-  secretKey: '6LfgipIUAAAAABljW3Zah2MB58dWBCu9srkeEljK' // retrieved during setup
-});*/
-
-
- 
-
-
 const app=express();
 var PORT=3000;
 
@@ -118,63 +108,6 @@ request(verifyUrl, (err, response, body) => {
 });
 });
 
-
-
-/*app.post('/send',function (req, res) {
-
-
-	var firstName=req.body.firstName;
-	var lastName=req.body.lastName;
-	var companyName=req.body.companyName;
-	var emailAdress=req.body.emailAddress;
-	var fromWhere=req.body.fromWhere;
-	var fromWhereOther=req.body.fromWhereOther;
-	var projectType=req.body.projectType;
-	var duration=req.body.duration;
-	var durationUnit=req.body.durationUnit;
-	var budget=req.body.budget;
-	var clientMsg=req.body.clientMsg;
-
-	console.log(req.body);
-
-	var transporter = nodemailer.createTransport({
-		service: 'Gmail',
-		auth: {
-			user: 'fastudyonlineflashcards@gmail.com',
-			pass: '2BZZ9i2nQnVcRyR'
-		}
-	});
-
-
-	transporter.sendMail({
-		from: emailAdress,
-		to: 'cmmccoy1996@gmail.com',
-		subject:"Message from "+firstName+" "+lastName,
-		text: 
-		'Company Name: '+companyName+"\n"+
-		'Email Address: '+emailAdress+"\n"+
-		'Project Type: '+projectType+"\n"+
-		'Duration: '+duration+" "+durationUnit+"\n"+
-		'Budget: '+budget+"\n"+
-		'From Where: '+fromWhere+"\n"+
-		'From Where Other: '+fromWhereOther+"\n"+
-		'Message: '+clientMsg+"\n",
-
-	
-	}, function(error, response){
-		if(error)
-		{
-			console.log(error);
-		}
-		else{
-			console.log("Message was sent.");
-		}
-	});
-
-
-	success=true;
-	res.redirect("/");
-});*/
 
 
 app.listen(PORT,function() {
